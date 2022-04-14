405388803354750e9dee0582e99ce82fbfc20323
Rebecca-Stone <primstone16@gmail.com>
2022-04-14 15:01:46 -0500

    setting project up

--- api/server.js
+++ api/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(cors());
+
+server.get("/", (req, res, next) => {
+  console.log('sanity check')
+});
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+require('dotenv').config();
+
+const server = require('./api/server');
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+    console.log(`\n***Server up on ${PORT}*** \n`);
+});
